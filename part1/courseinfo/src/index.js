3bb4bd45ffb54c559f55601c2f4f5d64d7fa82bd
Cedric Quenette <c@cedricq.me>
2020-12-24 12:27:10 +1100

    1.2: course information, step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Part = ({ part, exercises }) => {
+  return (
+    <>
+      <p>{part} {exercises}</p>
+    </>
+  )
+}
+
 const Header = ({ course }) => {
   return (
     <>
@@ -12,9 +20,9 @@ const Header = ({ course }) => {
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   )
 }
@@ -22,7 +30,7 @@ const Content = (props) => {
 const Total = ({ exercises }) => {
   return (
     <>
-      <p>Number of exercises </p>
+      <p>Number of exercises {exercises}</p>
     </>
   )
 }
