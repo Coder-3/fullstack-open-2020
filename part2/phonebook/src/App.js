31c5d108702dd3db9742f9cf7e468c9ca5af0ed1
Cedric Quenette <c@cedricq.me>
2021-01-02 22:08:18 +1100

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -15,7 +15,12 @@ const App = () => {
       name: newName,
     }
 
-    setPersons(persons.concat(personObject))
+    if(persons.map(person => person.name).includes(newName)) {
+      window.alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
+
     setNewName('')
   }
 
